f895d55792d058ad5adff9f80dd723e24804e0c1
mariuszchucherko <mariuszchucherko@workhuman.com>
2023-07-25 08:32:46 +0100

    38. JUnit Hello World Test

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,19 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+class GreetingTest {
+
+    @Test
+    void helloWorld() {
+        Greeting greeting = new Greeting();
+        System.out.println(greeting.helloWorld());
+
+    }
+
+    @Test
+    void testHelloWorld() {
+        Greeting greeting = new Greeting();
+        System.out.println(greeting.helloWorld("Mariusz"));
+    }
+}
